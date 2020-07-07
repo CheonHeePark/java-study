6367a5bcec2273795ebb8fc0bb818813e39ca419
chpark0904 <chpark0904@gmail.com>
2020-07-08 02:00:53 +0900

    takeWhile 주석처리했던 부분 출력되도록 변경(jdk9 설치했으므로)

--- modern-java/src/main/java/ch05/StreamSlicing.java
+++ modern-java/src/main/java/ch05/StreamSlicing.java
@@ -4,7 +4,8 @@ import ch04.Dish;
 import ch04.Menu;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class StreamSlicing {
     public static void main(String[] args) {
@@ -17,18 +18,20 @@ public class StreamSlicing {
     private static void onlyFilter() {
         Menu menu = new Menu();
         List<Dish> menus = menu.getMenus();
-        System.out.println(menus.toString());
+        System.out.println("모든 메뉴: " + menus.toString());
         List<Dish> filteredMenus = menus.stream()
                 .filter(dish -> dish.getCalories() < 320)
-                .collect(Collectors.toList());
-        System.out.println(filteredMenus.toString());
+                .collect(toList());
+        System.out.println("320칼로리 미만의 메뉴: " + filteredMenus.toString());
     }
 
     private static void withTakeWhile() {
         Menu menu = new Menu();
         List<Dish> menus = menu.getMenus();
-        System.out.println(menus.toString());
-       // List<Dish> filteredMenus = menus.stream()
-      //          .takeWhile(Dish::getCalories);
+        System.out.println("모든메뉴: " + menus.toString());
+        List<Dish> filteredMenus = menus.stream()
+                .takeWhile(dish -> dish.getCalories() > 500)
+                .collect(toList());
+        System.out.println("500칼로리 이상의 메뉴: " + filteredMenus);
     }
 }
